17823ecd91dbd9e06ece6387e4b566a43d337faf
James Lovallo <patrickonparker@gmail.com>
2021-10-17 16:31:52 -0400

    .

---
+++ src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+	<div
+		:class="`mdc-card ${blok.outlined ? 'mdc-card--outlined' : ''}`"
+		:style="blok.style"
+	>
+		<component
+			v-for="blok in blok.media"
+			:key="blok._uid"
+			:is="blok.component"
+			:blok="blok"
+		/>
+		<div class="mdc-card-wrapper__text-section">
+			<component
+				v-for="blok in blok.content"
+				:key="blok._uid"
+				:is="blok.component"
+				:blok="blok"
+			/>
+		</div>
+		<div class="mdc-card__actions">
+			<component
+				v-for="blok in blok.content"
+				:key="blok._uid"
+				:is="blok.actions"
+				:blok="blok"
+			/>
+		</div>
+	</div>
+</template>
+
+<script>
+import { Button } from "./Button.vue";
+export default {
+	props: ["blok"],
+	components: { Button },
+};
+</script>
---
+++ src/components/Children.vue
@@ -0,0 +1,14 @@
+<template>
+	<component
+		v-for="blok in source"
+		:key="blok._uid"
+		:is="blok.component"
+		:blok="blok"
+	/>
+</template>
+
+<script>
+import Button from "./Button.vue";
+import Card from "./Card.vue";
+export default { props: ["source"], components: { Button, Card } };
+</script>
