4b3feb17834713773c812b239120d4be625836a1
James Lovallo <patrickonparker@gmail.com>
2021-10-21 22:14:39 -0400

    add basic dynamic image loading

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,6 +1,20 @@
 export default function Image(props) {
 	const { blok } = props
-	const placeholder = `//picsum.photos/seed/${blok._uid}/400/300/`
+	let img = {}
+	if (blok.image.filename) {
+		let asset = blok.image.filename.split('.com')[1]
+		let dimensions = asset.split('/')[3]
+		img.src =
+			'https://img2.storyblok.com/widthx0/filters:format(webp):quality(80)' +
+			asset
+		img.width = dimensions.split('x')[0]
+		img.height = dimensions.split('x')[1]
+		img.paddingTop = (img.height / img.width) * 100 + '%'
+	} else return `https//picsum.photos/seed/${blok._uid}/400/300`
 
-	return <img src={placeholder} alt="" />
+	return (
+		<div className="image loading" style={{ paddingTop: img.paddingTop }}>
+			<img data-src={img.src} width={img.width} height={img.height} />
+		</div>
+	)
 }
