097ccb1b7dd05a31e0dff48694b40ed8737362f4
James Lovallo <patrickonparker@gmail.com>
2021-10-23 00:11:27 -0400

    .

--- src/components/CardChildren.jsx
+++ src/components/CardChildren.jsx
@@ -1,10 +1,12 @@
 import Button from './Button'
+import Image from './Image'
 import List from './List'
 import Map from './Map'
 import TextRich from './TextRich'
 
 const components = {
 	Button: Button,
+	Image: Image,
 	List: List,
 	Map: Map,
 	TextRich: TextRich,
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,26 +1,56 @@
 export default function Image(props) {
 	const { blok } = props
 	let img = {}
+	let [hor, vert] = [0, 0]
+
+	if (blok.aspect_ratio.match(/[0-9]+\/[0-9]+/)) {
+		;[hor, vert] = blok.aspect_ratio.split('/')
+	}
+
 	if (blok.image.filename) {
 		let asset = blok.image.filename.split('.com')[1]
 		let dimensions = asset.split('/')[3]
-		img.src =
-			'https://img2.storyblok.com/widthx0/filters:format(webp):quality(80)' +
-			asset
+
+		img.src = `https://img2.storyblok.com${
+			blok.fit === 'contain' ? '/fit-in' : ''
+		}/widthxheight${
+			blok.fit === 'smart' ? '/smart' : ''
+		}/filters:format(webp):quality(80):fill(transparent):focal(${
+			blok.image.focus
+		})${asset}`
+
 		img.width = dimensions.split('x')[0]
 		img.height = dimensions.split('x')[1]
-		img.paddingTop = (img.height / img.width) * 100 + '%'
-	} else return `https//picsum.photos/seed/${blok._uid}/400/300`
 
-	return (
-		<div className="image loading" style={{ paddingTop: img.paddingTop }}>
-			<img
-				src={img.src.replace('width', 30)}
-				data-src={img.src}
-				loading="lazy"
-				width={img.width}
-				height={img.height}
-			/>
-		</div>
-	)
+		if (hor && vert) {
+			img.height = (img.width * vert) / hor
+		}
+
+		const padding = Math.round((img.height / img.width) * 100) + '%'
+
+		img.styles = {
+			paddingTop: blok.fixed_width ? undefined : padding,
+			width: blok.fixed_width,
+			filter: `brightness(${blok.brightness})
+				contrast(${blok.contrast})
+				saturate(${blok.saturation})
+				grayscale(${blok.grayscale ? 1 : 0})`
+		}
+
+		img.tiny = img.src
+			.replace('width', img.hor * 2)
+			.replace('height', img.vert * 2)
+
+		return (
+			<div className="image loading" style={img.styles}>
+				<img
+					src={img.tiny || ''}
+					data-src={img.src}
+					loading="lazy"
+					width={img.width}
+					height={img.height}
+				/>
+			</div>
+		)
+	}
 }
