3528f445e403cfd9b24ac0ed1ebdb334cbee568a
James Lovallo <patrickonparker@gmail.com>
2021-10-23 19:33:33 -0400

    CardChildren -> Children

--- src/components/Children.jsx
+++ src/components/Children.jsx
@@ -12,7 +12,7 @@ const components = {
 	TextRich: TextRich,
 }
 
-export default function CardChildren(props) {
+export default function Children(props) {
 	const C = components[props.blok.component]
 	return <C blok={props.blok} />
 }
