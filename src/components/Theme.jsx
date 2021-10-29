a5b518bf3c8f903dc1db21362c166954dfdf44b5
James Lovallo <patrickonparker@gmail.com>
2021-10-28 21:22:58 -0400

    theme setting

---
+++ src/components/Theme.jsx
@@ -0,0 +1,20 @@
+export default function Theme(props) {
+	const { global } = props
+
+	const style = `
+		:root {
+			--background: ${global.background};
+			--on-background: ${global.on_background};
+			--primary: ${global.primary};
+			--on-primary: ${global.on_primary};
+			--secondary: ${global.secondary};
+			--on-secondary: ${global.on_secondary};
+			--surface: ${global.surface};
+			--on-surface: ${global.on_surface};
+			--surface-border: ${global.surface_border};
+		}
+
+		${global.custom_css}
+	`
+	return <style dangerouslySetInnerHTML={{ __html: style }}/>
+}
