bc8cb57310a216ddcf07d4708ae08c94cdbc0828
James Lovallo <patrickonparker@gmail.com>
2021-11-05 16:57:19 -0400

    .

--- src/components/utils/link.js
+++ src/components/utils/link.js
@@ -3,7 +3,7 @@ export default function link(link) {
 		switch (link.linktype) {
 			case 'story':
 				if (link.cached_url) {
-					return '/' + link.cached_url === 'home' ? '' : link.cached_url
+					return `/${link.cached_url === 'home' ? '' : link.cached_url}`
 				} else return undefined
 			case 'email':
 				return 'mailto:' + link.email
