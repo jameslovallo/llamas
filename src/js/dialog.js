06acb44d297f35bb744567354fa980f0fd415884
James Lovallo <kaina-agency@users.noreply.github.com>
2021-11-06 00:15:49 -0400

    disable scroll behind dialog

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -2,6 +2,7 @@ import MicroModal from 'micromodal'
 
 MicroModal.init({
     disableFocus: true,
+    disableScroll: true,
     onClose: function(modal, element, event) {
         event.preventDefault();
         event.stopPropagation();
