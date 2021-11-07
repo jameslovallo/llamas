1d29653da739ac7a46ce7043ddb125ef410f7d05
James Lovallo <patrickonparker@gmail.com>
2021-11-07 13:30:05 -0500

    swoop fix for iOS

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,6 +3,7 @@ import SectionChildren from './SectionChildren'
 export default function section({ blok }) {
 	return (
 		<div className={`section ${blok.style}`}>
+			{blok.style.includes('swoop') && <div className="section__top" />}
 			{blok.content.length > 0 && (
 				<div className="grid">
 					{blok.content.map((content) => (
